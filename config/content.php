f540bc5e0640c77e763528a0712dc89b6df1835b
Raqibul Hasan Moon <raqibul.hasan@ssd-tech.io>
2024-06-29 20:26:19 +0600

    init commit

---
+++ config/content.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    // Super Admin
+    'super_admin' => env('APP_SUPER_ADMIN') ?: 1,
+
+    // Admin Credentials
+    'admin_name' => env('ADMIN_NAME', 'admin'),
+    'admin_email' => env('ADMIN_EMAIL', 'admin@laravel-vue-admin.eu.org'),
+    'admin_password' => env('ADMIN_PASSWORD', '123456'),
+
+    // Default Avatar
+    'default_avatar' => env('DEFAULT_AVATAR', '/images/avatar.png'),
+
+    // Color Theme
+    'color_theme' => 'gray-theme',
+
+    // Meta
+    'meta' => [
+        'keywords' => 'Laravel Vue Admin',
+        'description' => 'Talk is cheap. Show me the code.'
+    ],
+
+    // Google Analytics
+    'google' => [
+        'id'   => env('GOOGLE_ANALYTICS_ID', 'Google-Analytics-ID'),
+        'open' => env('GOOGLE_OPEN') ?: false
+    ],
+
+    // Footer
+    'footer' => [
+        'github' => [
+            'open' => false,
+            'url'  => 'https://github.com/',
+        ],
+        'meta' => '© Laravel Vue Admin 2022.',
+    ],
+];
