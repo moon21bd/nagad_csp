5a11db027e314c8367fa62f853077d1c5dd12de6
SHARIF-HP\Sharif Mia <sharif.mia@ssd-tech.io>
2024-07-03 01:47:16 +0600

    First commit

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 
 import Router from "vue-router";
-import store from "./vuex";
 import AdminLayout from "./views/admin/layout/index";
+import store from "./vuex";
 
 Vue.use(Router);
 
@@ -12,33 +12,33 @@ let router = new Router({
         {
             path: "/",
             name: "home",
-            component: () => import("./views/home/index.vue")
+            component: () => import("./views/home/index.vue"),
         },
         {
             path: "/login/:user_id?",
             name: "login",
-            component: () => import("./views/login/index.vue")
+            component: () => import("./views/login/login.vue"),
         },
         {
             path: "/register",
             name: "register",
-            component: () => import("./views/register/index.vue")
+            component: () => import("./views/register/register.vue"),
         },
         {
             path: "/verify/user/:id",
             name: "verify",
             props: true,
-            component: () => import("./views/verify/index.vue")
+            component: () => import("./views/verify/verify.vue"),
         },
         {
             path: "/forgot-password",
             name: "forgot",
-            component: () => import("./views/forgot/index.vue")
+            component: () => import("./views/forgot/forgot.vue"),
         },
         {
             path: "/reset/:token",
             name: "reset",
-            component: () => import("./views/reset/index.vue")
+            component: () => import("./views/reset/index.vue"),
         },
         /**
          * Admin routes
@@ -49,8 +49,8 @@ let router = new Router({
             component: () => import("./views/admin/dashboard.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/components/buttons",
@@ -58,8 +58,8 @@ let router = new Router({
             component: () => import("./views/admin/buttons.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/components/cards",
@@ -67,8 +67,8 @@ let router = new Router({
             component: () => import("./views/admin/cards.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/utilities/colors",
@@ -76,8 +76,8 @@ let router = new Router({
             component: () => import("./views/admin/colors.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/utilities/borders",
@@ -85,8 +85,8 @@ let router = new Router({
             component: () => import("./views/admin/borders.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/utilities/animations",
@@ -94,8 +94,8 @@ let router = new Router({
             component: () => import("./views/admin/animations.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/utilities/other",
@@ -103,8 +103,8 @@ let router = new Router({
             component: () => import("./views/admin/other.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/pages/page-not-found",
@@ -112,8 +112,8 @@ let router = new Router({
             component: () => import("./views/admin/page-not-found.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/pages/blank",
@@ -121,8 +121,8 @@ let router = new Router({
             component: () => import("./views/admin/blank.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/charts",
@@ -130,8 +130,8 @@ let router = new Router({
             component: () => import("./views/admin/charts.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
+                layout: AdminLayout,
+            },
         },
         {
             path: "/admin/tables",
@@ -139,14 +139,14 @@ let router = new Router({
             component: () => import("./views/admin/tables.vue"),
             meta: {
                 requiresAuth: true,
-                layout: AdminLayout
-            }
-        }
-    ]
+                layout: AdminLayout,
+            },
+        },
+    ],
 });
 
 router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
         if (store.getters.user) {
             next();
             return;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,14 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" type="text/css">
 
     <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
