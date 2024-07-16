5ea608900d1ba1b372d4f172df7a51f781b86dd0
SHARIF-HP\Sharif Mia <sharif.mia@ssd-tech.io>
2024-07-17 01:36:24 +0600

    permission assign design

---
+++ resources/js/components/PermissionsAssign.vue
@@ -0,0 +1,148 @@
+<template>
+    <div>
+        <div class="common-heading d-flex align-items-center mb-3">
+            <h1 class="title m-0">Permissions Assign</h1>
+        </div>
+        <div class="card mb-4">
+            <div class="overlay" v-if="isLoading">
+                <img src="/images/loader.gif" alt="" />
+            </div>
+            <div class="card-body">
+                <div class="permissions-assign">
+                    <div class="permissions-assign-box">
+                        <h4>
+                            <label class="checkbox"
+                                ><input
+                                    type="checkbox"
+                                    name="permissions"
+                                /><span class="checkmark"></span>User</label
+                            >
+                        </h4>
+                        <ul>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Create</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Update</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Delete</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User Status
+                                    Change</label
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="permissions-assign-box">
+                        <h4>
+                            <label class="checkbox"
+                                ><input
+                                    type="checkbox"
+                                    name="permissions"
+                                /><span class="checkmark"></span>User</label
+                            >
+                        </h4>
+                        <ul>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Create</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Update</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Delete</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User Status
+                                    Change</label
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="permissions-assign-box">
+                        <h4>
+                            <label class="checkbox"
+                                ><input
+                                    type="checkbox"
+                                    name="permissions"
+                                /><span class="checkmark"></span>User</label
+                            >
+                        </h4>
+                        <ul>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Create</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Update</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User
+                                    Delete</label
+                                >
+                            </li>
+                            <li>
+                                <label class="checkbox">
+                                    <input type="checkbox" name="permissions" />
+                                    <span class="checkmark"></span>User Status
+                                    Change</label
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PermissionsAssign",
+    data() {
+        return {
+            isLoading: false,
+        };
+    },
+    methods: {},
+};
+</script>
--- resources/js/routers/roles-permissions.js
+++ resources/js/routers/roles-permissions.js
@@ -1,9 +1,10 @@
 import AdminLayout from "../views/admin/layout/index.vue";
 
-const Profile = () => import('../components/Profile.vue')
-const Roles = () => import('../components/Roles.vue')
-const Permissions = () => import('../components/Permissions.vue')
-const Users = () => import('../components/Users.vue')
+const Profile = () => import("../components/Profile.vue");
+const Roles = () => import("../components/Roles.vue");
+const Permissions = () => import("../components/Permissions.vue");
+const PermissionsAssign = () => import("../components/PermissionsAssign.vue");
+const Users = () => import("../components/Users.vue");
 
 export default [
     {
@@ -11,43 +12,54 @@ export default [
         path: "/admin/profile",
         component: Profile,
         meta: {
-            title: 'Profile',
+            title: "Profile",
             middleware: "auth",
             requiresAuth: true,
-            layout: AdminLayout
-        }
+            layout: AdminLayout,
+        },
     },
     {
         name: "roles",
         path: "/admin/roles",
         component: Roles,
         meta: {
-            title: 'Roles',
+            title: "Roles",
             middleware: "auth",
             requiresAuth: true,
             layout: AdminLayout,
-        }
+        },
     },
     {
         name: "permissions",
         path: "/admin/permissions",
         component: Permissions,
         meta: {
-            title: 'Permissions',
+            title: "Permissions",
             middleware: "auth",
             requiresAuth: true,
             layout: AdminLayout,
-        }
+        },
+    },
+    {
+        name: "permissionsAssign",
+        path: "/admin/permissions-assign",
+        component: PermissionsAssign,
+        meta: {
+            title: "PermissionsAssign",
+            middleware: "auth",
+            requiresAuth: true,
+            layout: AdminLayout,
+        },
     },
     {
         name: "users",
         path: "/admin/users",
         component: Users,
         meta: {
-            title: 'Users',
+            title: "Users",
             middleware: "auth",
             requiresAuth: true,
-            layout: AdminLayout
-        }
-    }
-]
+            layout: AdminLayout,
+        },
+    },
+];
