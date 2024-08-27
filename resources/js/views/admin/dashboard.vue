1374581171b6e52a06fb735524e9f32bc2df9886
moon21bd <rhmoon21@gmail.com>
2024-08-27 15:39:57 +0600

    dashboard states default value modified.

--- resources/js/views/admin/dashboard.vue
+++ resources/js/views/admin/dashboard.vue
@@ -203,9 +203,9 @@ export default {
         return {
             monthValue: "",
             monthTickets: "",
-            totalReportCount: "",
-            dailyReportCount: "",
-            monthWiseReportCount: "",
+            totalReportCount: {},
+            dailyReportCount: {},
+            monthWiseReportCount: {},
             chartInstance: null,
             userGroupId: 1,
             columnChartData: {
